fcbd5fad9f0b3d5cc952569280b0ef693fb70347
Chae Hui Seon <smilechs34@gmail.com>
2021-06-05 23:53:04 +0900

    인자수정

--- textrank.py
+++ textrank.py
@@ -1,3 +1,4 @@
+
 from typing import List, Callable, Union, Any, TypeVar, Tuple, Dict
 from functools import partial
 from sklearn.feature_extraction.text import CountVectorizer
@@ -6,8 +7,10 @@ from sklearn.preprocessing import normalize
 import numpy as np
 
 class TextRank:
-    
-    def vectorize_sents(corp_doc_topic = corp_doc_topic, min_count=2, tokenizer="mecab", noun=True):
+    def __init__(self,corp_doc_topic):
+        self.corp_doc_topic = corp_doc_topic
+
+    def vectorize_sents(self,corp_doc_topic, min_count=2, tokenizer="mecab", noun=True):
         vectorizer = CountVectorizer(tokenizer=lambda x: x,lowercase=False)
         vec = vectorizer.fit_transform(corp_doc_topic)
         vocab_idx = vectorizer.vocabulary_
@@ -15,31 +18,23 @@ class TextRank:
 
         return vec, vocab_idx, idx_vocab
 
-    def word_similarity_matrix(x, min_sim=0.3):
+    def word_similarity_matrix(self,x, min_sim=0.3):
         #문장 간 유사도가 0.3 보다 작은 경우에는 edge 를 연결지 않으면서 단어 그래프 생성.
         sim_mat = 1 - pairwise_distances(x.T, metric="cosine")
         sim_mat[np.where(sim_mat <= min_sim)] = 0
 
         return sim_mat
 
-    
 
+    def word_graph(self,corp_doc_topic ,min_count=5,min_sim=0.3,tokenizer="mecab",noun=True):
 
+        mat, vocab_idx, idx_vocab = self.vectorize_sents(
+            corp_doc_topic,min_count=min_count, tokenizer=tokenizer, noun=noun)
 
-
-    def word_graph(corp_doc_topic = corp_doc_topic,min_count=5,min_sim=0.3,tokenizer="mecab",noun=True,):
-
-        mat, vocab_idx, idx_vocab = vectorize_sents(
-            corp_doc_topic,min_count=min_count, tokenizer=tokenizer, noun=noun
-        )
-
-        mat = word_similarity_matrix(mat, min_sim=min_sim)
+        mat = self.word_similarity_matrix(mat, min_sim=min_sim)
         return mat, vocab_idx, idx_vocab
-    
-
-
-    
-    def pagerank(x: np.ndarray, df=0.85, max_iter=50):
+     
+    def pagerank(self,mat: np.ndarray, df=0.85, max_iter=50):
 
         assert 0 < df < 1
 
@@ -53,19 +48,28 @@ class TextRank:
 
         return R
 
-
-
-    def keyword_extraxtor(corp_doc_topic):
+    def keyword_extraxtor(self):
         
         keywords = []
-        for i in range(len(corp_doc_topic)):
-            if len(corp_doc_topic[i])!= 0:
-                mat,vocab_idx, idx_vocab = word_graph(corp_doc_topic[i])
-                R = pagerank(mat)
+        for i in range(len(self.corp_doc_topic)):
+            if len(self.corp_doc_topic[i])!= 0:
+                mat,vocab_idx, idx_vocab = self.word_graph(self.corp_doc_topic[i])
+                R = self.pagerank(mat)
                 topk = 10
                 idxs = R.argsort()[-topk:]
                 keywords.append([(idx, R[idx], idx_vocab[idx]) for idx in reversed(idxs)])
                 #keyword = [(R[idx]) for idx in reversed(idxs)]
         
         return keywords
+
+    def save_topics(self):
+        ext_topic_cluster = dict()
+        tc = []
+        for i in range(1,NUM_TOPICS):
+            top_save = dict()
+            for j in range(len(topic_docs_save[i])):
+                top_save[topic_docs_save[i][j][0]] = topic_docs_save[i][j][1]
+            ext_topic_cluster[i] = [keywords[i-1],top_save]
+        return ext_topic_cluster, NUM_TOPICS
+
     
